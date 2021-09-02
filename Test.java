cdb369319d77dc9c727db66685679b1ecb5763ba
Faizan Azim <faizanazim11@gmail.com>
2021-09-03 00:56:39 +0530

    Changes

--- Test.java
+++ Test.java
@@ -8,9 +8,11 @@ public class Test
     public static void view()
     {
         //TO-DO @faizanazim11
+        System.out.println("World");
     }
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        view();
     }
 }
