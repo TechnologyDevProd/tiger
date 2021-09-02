02c3b821ab5064cdcd73070a237738feeeb52245
Faizan Azim <faizanazim11@gmail.com>
2021-09-03 00:47:53 +0530

    Test java commit

---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test
+{
+    public static void display()
+    {
+        //TO-DO @TechnologyDevProd
+    }
+
+    public static void view()
+    {
+        //TO-DO @faizanazim11
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
